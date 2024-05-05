7df16f7a8bf6c02eba76b951c81723075bd455e4
Frederick Emerson <46756791+frederickemerson@users.noreply.github.com>
2024-05-05 09:56:24 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -38,14 +38,14 @@ export default function Home() {
           <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Feeling <span className="text-[hsl(280,100%,70%)]">Lonely</span> ?
           </h1>
-          <div className="w-1/2 justify-center items-center flex flex-row ">
+          <div className="w-1/2 justify-center items-center flex flex-col ">
           <Input onChangeCapture={e => setEmail(e.currentTarget.value)}
-        className="flex-1 w-3/4 mx-4 rounded-md border border-gray-300 px-4 py-2 text-sm focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-gray-600 dark:focus:ring-gray-600"
+        className="flex-1 w-full mx-4 rounded-md border border-gray-300 px-4 py-2 text-sm focus:border-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-500 dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-gray-600 dark:focus:ring-gray-600"
         placeholder="Enter your email"
         type="email"
       />
       <Button onClick={handleSubmit}
-        className="inline-flex h-10 w-1/4 items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white shadow-sm transition-colors hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:ring-gray-600 dark:focus:ring-offset-gray-900"
+        className="inline-flex h-10 w-full items-center justify-center rounded-md bg-gray-900 px-4 py-2 text-sm font-medium text-white shadow-sm transition-colors hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-200 dark:focus:ring-gray-600 dark:focus:ring-offset-gray-900"
         type="submit"
       >
         Get A Hi 👋
