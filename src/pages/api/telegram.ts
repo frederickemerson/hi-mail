3ad25d6b31f7d29e3d2c36d0a4b446b622a4e33e
Frederick Emerson <frederick82004@gmail.com>
2024-05-05 12:52:00 +0800

    trying out more

--- src/pages/api/telegram.ts
+++ src/pages/api/telegram.ts
@@ -12,8 +12,10 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
     
         const {message} = req.body as {message:Message};
-        if (message.text === '/start') {
-            const tex = "hi"
+        if (message.text == '/start') {
+            const tex =       'Welcome to <i>Authifier</i> <b>' +
+            req.body.message.from.first_name +
+            '</b>.%0ATo get a list of commands sends /help';
         const ret = await fetch(
             `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${message.chat.id}&text=${tex}&parse_mode=HTML`
           );
