a20b0a633684a7ea128f5bfede18b001c1bc22ca
Frederick Emerson <46756791+frederickemerson@users.noreply.github.com>
2024-05-05 09:58:02 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]">
             Feeling <span className="text-[hsl(280,100%,70%)]">Lonely</span> ?
           </h1>
           <div className="w-1/2 justify-center items-center flex flex-col ">
