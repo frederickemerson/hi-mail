d7bbf8f5be01762d293ac13a15c0c7af411e2ea7
Frederick Emerson <frederick82004@gmail.com>
2024-05-05 13:19:08 +0800

    finally i think im done with this

--- src/pages/api/telegram.ts
+++ src/pages/api/telegram.ts
@@ -12,14 +12,23 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         }
     
         const {message} = req.body as {message:Message};
-        if (message.text == '/start') {
-            const tex =       'Welcome to <i>Authifier</i> <b>' +
-            message?.from?.first_name+
-            '</b>.%0ATo get a list of commands sends /help';
-        const ret = await fetch(
-            `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${message.chat.id}&text=${tex}&parse_mode=HTML`
-          );
-        }
+
+        if (message.text  === '/start') {
+            const tex =       'Welcome to <i>Authifier</i> <b>' + message?.from?.first_name+ '</b>.%0AUse /main to get started';
+            await sendMessage(message.chat.id, tex);
+          }  
+        else if (message.text  === '/main') {
+            const tex = "Open the Web Mini App:";
+          
+            const keyboard = JSON.stringify({
+              inline_keyboard: [[{
+                text: "Open Web App",
+                web_app: { url: "https://t.me/authifier_bot/authify" }
+              }]]
+            });
+            await sendMessage(message.chat.id, tex, keyboard);
+          }
+        
         console.log(message)
       res.status(200).send('OK');
     }
@@ -28,3 +37,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         res.status(500).send('Error');
       }
 }
+
+async function sendMessage(chatId:number, text:string, replyMarkup = '') {
+    const encodedText = encodeURIComponent(text);
+    const url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage?chat_id=${chatId}&text=${encodedText}&parse_mode=HTML`;
+  
+    if (replyMarkup) {
+      await fetch(`${url}&reply_markup=${encodeURIComponent(replyMarkup)}`);
+    } else {
+      await fetch(url);
+    }
+  }
